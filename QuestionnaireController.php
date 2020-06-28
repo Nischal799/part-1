f9ce9612a34380c8c4a55964804a3fe58fded34f
Nischal799 <66230664+Nischal799@users.noreply.github.com>
2020-06-28 21:53:11 +0530

    Add files via upload

---
+++ QuestionnaireController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class QuestionnaireController extends Controllers
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function create()
+    {
+        return view('Questionnaire.create');
+    }
+
+    public function store() 
+    {
+        $data = request()->validate([
+            'title' => 'required',
+            'purpose' => 'required',
+        ]);
+
+            $questionnaire = auth()->user()->questionnaire()->create($data);
+
+        return redirect('/Questionnaires/'.$questionnaire->id);
+    }  
+    
+    public function show(\App\Questionnaire_$questionnaire)
+    {
+        return view('Questionnaire.show', compact('questionnaire'));
+    }
+}
